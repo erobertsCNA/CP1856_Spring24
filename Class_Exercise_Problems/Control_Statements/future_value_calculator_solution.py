d30cc0bea0dcd9cea73fcff5cd479410bc82e6f4
arun-rameshbabu <arun.rameshbabu@cna.nl.ca>
2024-05-23 16:01:26 -0230

    Week-3-Class-2 Interim commit
    More practice problems and solution for future value calculator.

--- Class_Exercise_Problems/Control_Statements/future_value_calculator_solution.py
+++ Class_Exercise_Problems/Control_Statements/future_value_calculator_solution.py
@@ -22,3 +22,26 @@ run a for loop for the range(number of months)
 print final sum (future value)
 """
 
+print("Welcome to the Future Value Calculator\n")
+choice = 'y'
+while choice.lower() == 'y':
+    
+    # Gathering inputs
+    monthly_investment = float(input("Enter monthly investment:\t\t"))
+    intrest_rate_yr = float(input("Enter yearly interest rate:\t\t"))
+    num_years = float(input("Enter the number of years:\t\t"))
+    
+    # Do the calculations
+    future_value = 0
+    interest_rate_monthly = (intrest_rate_yr/12)/100
+    num_months = num_years * 12
+    
+    # Loop through
+    for mnth in range(int(num_months)):
+        future_value += monthly_investment # Add new investment
+        interest_for_mnth = future_value * interest_rate_monthly # Calculate interest for month
+        future_value += interest_for_mnth # Add interest to the principal
+    
+    # Print the outputs
+    print(f"Future Value:\t\t\t\t\t{future_value:.2f}\n")
+    choice = input("Continue (y/n)?:")
