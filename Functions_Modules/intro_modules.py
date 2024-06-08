9281ca9343e4fd6148779407037b45b2458f1084
arun-rameshbabu <arun.rameshbabu@cna.nl.ca>
2024-06-08 17:42:10 -0230

    End of Week 4 Class 3

---
+++ Functions_Modules/intro_modules.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 15:36:51 2024
+
+@author: Arun_Rameshbabu
+"""
+
+import intro_functions as intro
+
+intro.print_welcome("Hey, I'm in the intro_modules file")
+intro.miles_per_gallon(100,10)
+
+# intro.main()
+print(intro.__name__)
+print(__name__)
+
+help(intro)
