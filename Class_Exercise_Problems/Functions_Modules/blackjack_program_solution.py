292fa4d4f6c2efd12452b1a2321d476c520aa88e
arun-rameshbabu <arun.rameshbabu@cna.nl.ca>
2024-06-12 17:09:57 -0230

    End of Week 6 Class 1
    Blackjack full solution and data importer module.

--- Class_Exercise_Problems/Functions_Modules/blackjack_program_solution.py
+++ Class_Exercise_Problems/Functions_Modules/blackjack_program_solution.py
@@ -14,8 +14,30 @@ number 58 - 94 -> Win
 number 49 - 57 -> Push
 number < 49 -> Lose
 """
+def menu():
+    """
+    Prints menu
+
+    Returns
+    -------
+    None.
+
+    """
+    print("BLACKJACK!")
+    print("Blackjack payout is 3:2")
+    print("Enter 'x' for bet to exit")
+
 
 def bwpl():
+    """
+    Logic to determine blackjack, win, push or lose
+
+    Returns
+    -------
+    str
+        State of the play.
+
+    """
     num = random.randint(1, 100)
     if num > 95:
         return 'b'
@@ -26,4 +48,38 @@ def bwpl():
     else:
         return 'l'
 
-print(bwpl())
+
+def main():
+    """
+    Tha main function for the blackjack game
+
+    Returns
+    -------
+    None.
+
+    """
+    starting_money = float(input("Starting money: "))
+    while starting_money > 0:
+        bet_amount = input("\nBet amount: ")
+        if bet_amount == 'x':
+            break
+        else:
+            bet_amount = int(bet_amount)
+            game_state = bwpl()
+            if game_state == 'b':
+                print("You got a blackjack!")
+                starting_money += 1.5 * bet_amount
+            elif game_state == 'w':
+                print("You won.")
+                starting_money += bet_amount
+            elif game_state == 'p':
+                print("You pushed.")
+            else:
+                print("You lost.")
+                starting_money -= bet_amount
+            print(f"Money: {starting_money}")
+    print("Bye!")
+
+if __name__ == "__main__":
+    menu()
+    main()
---
+++ Class_Exercise_Problems/Functions_Modules/data_importer_program_solution.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 12 16:57:24 2024
+
+@author: Arun.Rameshbabu
+"""
+
+import sales
+help(sales)
